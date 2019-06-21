7cf098347a1c8088f109aad67760268dc6be2d36
Seil Cho <seilcho7@hotmail.com>
2019-06-21 13:41:31 -0400

    finish codewars array question

---
+++ codewars/arrays1.js
@@ -0,0 +1,15 @@
+// Create a function that returns the sum of the two lowest positive numbers given an array of minimum 4 positive integers. No floats or non-positive integers will be passed.
+// For example, when an array is passed like [19, 5, 42, 2, 77], the output should be 7.
+// [10, 343445353, 3453445, 3453545353453] should return 3453455
+
+
+const numbers = [10, 343445353, 3453445, 3453545353453];
+
+function sumTwoSmallestNumbers(numbers) {  
+    const sortedNumbers = numbers.sort(function (a, b) { return a-b });
+    let sum = 0;
+    sum = sortedNumbers[0] + sortedNumbers[1];
+    return sum;
+}
+
+sumTwoSmallestNumbers(numbers);
