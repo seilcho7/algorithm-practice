6a03f53d77b9fb0825c57f99f860a8b13b056ce6
Seil Cho <seilcho7@hotmail.com>
2019-07-07 17:25:54 -0400

    solved hackerrank alternating characters problem

--- hackerrank/arrays-2.js
+++ hackerrank/arrays-2.js
@@ -4,9 +4,11 @@ const a = [1,2,3,4,5];
 const b = 4;
 
 function rotLeft(a, d) {
+    let numb = "1,2,3,4,5".split(',').map(Number);
+    console.log(numb);
     for (let i = 0; i < d; i++) {
         a.push(...a.splice(0,1));
-        console.log
+        // console.log(a);
     }
     console.log(a);
 
